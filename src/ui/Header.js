4463513b54b5d81dd4e59a5e59fda516a36ec7ae
rupeez7 <rupeshxtha6@gmail.com>
2024-03-31 18:03:34 +0545

    update

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -3,9 +3,11 @@ import { NavLink } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div className='bg-black text-white px-5 flex items-baseline justify-between py-2'>
+    <div className='bg-black text-white border border-white px-5 flex items-baseline justify-between py-2 sticky top-0 z-50'>
+
       <h1>Rupesh Shrestha</h1>
       <nav className='space-x-5 '>
+        <NavLink to={'/'}>Home</NavLink>
         <NavLink to={'/about'}>About</NavLink>
         <NavLink to={'/content'}>Contact</NavLink>
       </nav>
